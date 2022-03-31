284094fee7f57c67aff9b7b46f46105c50676bd2
szz <67**37@qq.com>
2022-03-31 00:14:51 -0700

    base_dir和debug

--- bookmanager/bookmanager/settings.py
+++ bookmanager/bookmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6muc61bf67k8h5*tlean%58hl%$2w0)-q_%ze83$&l10se+&7x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
